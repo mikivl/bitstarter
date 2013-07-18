8ac7a00f6108be8f5e01a90ef261b61e7910e3b9
mikivl <milan.vlah@gmail.com>
2013-07-19 01:15:10 +0200

    Modified web.js string from index.html

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
 	var mybuff = fs.readFileSync("index.html");
 	var mystr = mybuff.toString();
